da621f35a4ba511d7f4641af9536b5366efa6212
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-09-25 16:40:53 +0530

    new project

---
+++ script.js
@@ -0,0 +1,48 @@
+const accessKey = "029td0m2aKmwAo0m9L_V-dd_HGxlww_XA6z3oZ7oiJ8";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+
+    showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener("click", ()=> {
+    page++;
+    searchImages();
+})
+
